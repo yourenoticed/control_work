936c476e166cf5dbff07670f9244365f4f7ce40c
yourenoticed <yourenoticed@gmail.com>
2023-08-02 18:40:32 +0700

    wrote the program

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿string[] ElementsWithLengthLessThan4(string[] elements)
+{
+    int size = 0;
+    // finding the length of output array
+    foreach (string element in elements)
+    {
+        if (element.Length <= 3)
+        {
+            size++;
+        }
+    }
+
+    // creating a new array, filling it with corresponding elements
+    string[] result = new string[size];
+    int i = 0;
+    foreach (string element in elements)
+    {
+        if (element.Length <= 3)
+        {
+            result[i] = element;
+            i++;
+        }    
+    }        
+    return result;
+}
+
+string[] arr = {"Hello", "2", "world", ":-)"};
+string[] result = ElementsWithLengthLessThan4(arr);
+foreach (string element in result)
+{
+    System.Console.WriteLine(element);
+}
